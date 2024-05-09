654789a4c3c2aeb0fa8790393f5c67f7aae657dc
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-05-09 12:45:30 +0600

    core message lang file moved to rest api

--- src/Models/Trigger.php
+++ src/Models/Trigger.php
@@ -79,10 +79,10 @@ class Trigger extends BaseModel
         $primaryKey = $this->getKey();
 
         $links = [
-            'show' => action_link(route('bell.triggers.show', $primaryKey), __('core::messages.action.show'), 'get'),
-            'update' => action_link(route('bell.triggers.update', $primaryKey), __('core::messages.action.update'), 'put'),
-            'destroy' => action_link(route('bell.triggers.destroy', $primaryKey), __('core::messages.action.destroy'), 'delete'),
-            'restore' => action_link(route('bell.triggers.restore', $primaryKey), __('core::messages.action.restore'), 'post'),
+            'show' => action_link(route('bell.triggers.show', $primaryKey), __('restapi::messages.action.show'), 'get'),
+            'update' => action_link(route('bell.triggers.update', $primaryKey), __('restapi::messages.action.update'), 'put'),
+            'destroy' => action_link(route('bell.triggers.destroy', $primaryKey), __('restapi::messages.action.destroy'), 'delete'),
+            'restore' => action_link(route('bell.triggers.restore', $primaryKey), __('restapi::messages.action.restore'), 'post'),
         ];
 
         if ($this->getAttribute('deleted_at') == null) {
