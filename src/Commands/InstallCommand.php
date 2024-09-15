4dece998242c873fcd5c5b54ea57b4376d5cd8eb
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-09-16 02:44:30 +0600

    installer optimized

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -2,17 +2,26 @@
 
 namespace Fintech\Bell\Commands;
 
+use Fintech\Core\Traits\HasCoreSettingTrait;
 use Illuminate\Console\Command;
 
 class InstallCommand extends Command
 {
+    use HasCoreSettingTrait;
+
     public $signature = 'bell:install';
 
-    public $description = 'My command';
+    public $description = 'Configure the system for the `fintech/bell` module';
+
+    private string $module = 'Bell';
 
     public function handle(): int
     {
-        $this->comment('All done');
+        $this->infoMessage("Module Installation", 'RUNNING');
+
+        $this->task("Module Installation", function () {
+
+        }, 'COMPLETED');
 
         return self::SUCCESS;
     }
