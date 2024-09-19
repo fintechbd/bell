ab4bef36315a8d9c935de8ac20b6f785e63388b2
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-09-20 02:37:56 +0600

    unused php doc block removed

--- src/Bell.php
+++ src/Bell.php
@@ -27,41 +27,26 @@ class Bell
         return \singleton($driver);
     }
 
-    /**
-     * @return TriggerService
-     */
     public function trigger($filters = null)
     {
         return \singleton(TriggerService::class, $filters);
     }
 
-    /**
-     * @return TriggerRecipientService
-     */
     public function triggerRecipient($filters = null)
     {
         return \singleton(TriggerRecipientService::class, $filters);
     }
 
-    /**
-     * @return TriggerVariableService
-     */
     public function triggerVariable($filters = null)
     {
         return \singleton(TriggerVariableService::class, $filters);
     }
 
-    /**
-     * @return NotificationTemplateService
-     */
     public function notificationTemplate($filters = null)
     {
         return \singleton(NotificationTemplateService::class, $filters);
     }
 
-    /**
-     * @return TriggerActionService
-     */
     public function triggerAction($filters = null)
     {
         return \singleton(TriggerActionService::class, $filters);
