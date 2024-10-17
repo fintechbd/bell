d1ba99a565ba7209325decbc801898ed0b44d06d
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-10-17 15:31:37 +0600

    break: bell module audit tweak added

--- src/Models/NotificationTemplate.php
+++ src/Models/NotificationTemplate.php
@@ -4,12 +4,15 @@ namespace Fintech\Bell\Models;
 
 use Fintech\Core\Abstracts\BaseModel;
 use Fintech\Core\Traits\AuditableTrait;
+use Fintech\Core\Traits\BlameableTrait;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class NotificationTemplate extends BaseModel
+class NotificationTemplate extends BaseModel implements Auditable
 {
-    use AuditableTrait;
+    use \OwenIt\Auditing\Auditable;
+    use BlameableTrait;
     use SoftDeletes;
 
     /*
--- src/Models/Trigger.php
+++ src/Models/Trigger.php
@@ -4,12 +4,15 @@ namespace Fintech\Bell\Models;
 
 use Fintech\Core\Abstracts\BaseModel;
 use Fintech\Core\Traits\AuditableTrait;
+use Fintech\Core\Traits\BlameableTrait;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class Trigger extends BaseModel
+class Trigger extends BaseModel implements Auditable
 {
-    use AuditableTrait;
+    use \OwenIt\Auditing\Auditable;
+    use BlameableTrait;
     use SoftDeletes;
 
     /*
