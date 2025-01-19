e44e5ad62adf3d30375f0ff12cf69a3465c89cb7
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-01-19 20:17:55 +0600

    event meta data added

--- src/Services/TriggerService.php
+++ src/Services/TriggerService.php
@@ -2,9 +2,6 @@
 
 namespace Fintech\Bell\Services;
 
-use Fintech\Auth\Events\Attempting;
-use Fintech\Auth\Events\Failed;
-use Fintech\Auth\Events\Lockout;
 use Fintech\Core\Attributes\ListenByTrigger;
 use Fintech\Core\Listeners\TriggerNotification;
 use Illuminate\Support\Collection;
@@ -62,18 +59,26 @@ class TriggerService
             $triggers = collect();
 
             foreach ($eventDispatcher->getRawListeners() as $event => $listeners) {
-                $reflector = new ReflectionClass($event);
-                if (in_array(TriggerNotification::class, $listeners) && !empty($reflector->getAttributes(ListenByTrigger::class))) {
-                    $triggerInfo = $reflector->getAttributes(ListenByTrigger::class)[0]->newInstance();
-                    $triggers->push([
-                        'id' => Str::uuid()->toString(),
-                        'name' => $triggerInfo->name(),
-                        'code' => $event,
-                        'description' => $triggerInfo->description(),
-                        'enabled' => $triggerInfo->enabled(),
-                        'variables' => array_map(fn($variable) => ['name' => $variable->name(), 'description' => $variable->description()], $triggerInfo->variables())
-                    ]);
+                if (!in_array(TriggerNotification::class, $listeners)) {
+                    continue;
                 }
+
+                $reflector = new ReflectionClass($event);
+
+                if (empty($reflector->getAttributes(ListenByTrigger::class))) {
+                    continue;
+                }
+
+                $triggerInfo = $reflector->getAttributes(ListenByTrigger::class)[0]->newInstance();
+
+                $triggers->push([
+                    'id' => Str::uuid()->toString(),
+                    'name' => $triggerInfo->name(),
+                    'code' => $event,
+                    'description' => $triggerInfo->description(),
+                    'enabled' => $triggerInfo->enabled(),
+                    'variables' => array_map(fn($variable) => ['name' => $variable->name(), 'description' => $variable->description()], $triggerInfo->variables())
+                ]);
             }
 
             return $triggers;
