8ee59f6d251d050f5952a4b9e677eacf444f5d47
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-02-06 01:00:06 +0600

    notification seeder updated

--- database/seeders/TemplateSeeder.php
+++ database/seeders/TemplateSeeder.php
@@ -12,18 +12,17 @@ class TemplateSeeder extends Seeder
      */
     public function run(): void
     {
-        $data = $this->data();
-
-        foreach (array_chunk($data, 200) as $block) {
-            set_time_limit(2100);
-            foreach ($block as $entry) {
-                Bell::template()->create($entry);
-            }
+        foreach ($this->data() as $entry) {
+            Bell::template()->create($entry);
         }
     }
 
     private function data()
     {
-        return [];
+        return [
+            [
+
+            ]
+        ];
     }
 }
