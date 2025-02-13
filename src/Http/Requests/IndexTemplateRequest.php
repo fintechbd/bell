a9b7b2c86d210bec78d8f7914acb1afc381ba1c1
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-02-13 17:01:11 +0600

    otp size extended to 6 digit

--- src/Http/Requests/IndexTemplateRequest.php
+++ src/Http/Requests/IndexTemplateRequest.php
@@ -26,7 +26,7 @@ class IndexTemplateRequest extends FormRequest
     {
         return [
             'search' => ['string', 'nullable', 'max:255'],
-            'per_page' => ['integer', 'nullable', 'min:10', 'max:500'],
+            'per_page' => ['integer', 'nullable', 'min:1', 'max:500'],
             'page' => ['integer', 'nullable', 'min:1'],
             'paginate' => ['boolean'],
             'sort' => ['string', 'nullable', 'min:2', 'max:255'],
